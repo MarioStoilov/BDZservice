73016f5d939878f51b39617c96739b4a1341b8df
MarioStoilov <stoilov_mario@yahoo.com>
2013-09-03 22:41:44 +0300

    added privacy policy

---
+++ BDZService/BDZService/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BDZService.Controllers
+{
+    public class PrivacyPolicyController : Controller
+    {
+        //
+        // GET: /PrivacyPolicy/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

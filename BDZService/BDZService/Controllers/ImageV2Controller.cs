2dda8b3e376cb435ec08973bd64b4fae576ed51f
Vlado Miloshev <vladimir.miloshev@sirma.bg>
2013-12-10 15:51:50 +0200

    Added JSON controllers for bad endpoints

---
+++ BDZService/BDZService/Controllers/ImageV2Controller.cs
@@ -0,0 +1,20 @@
+﻿using BDZWebsiteUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BDZService.Controllers
+{
+    public class ImageV2Controller : ApiController
+    {
+        // GET api/imageV2
+        public string Post([FromBody]ImageDTO imageDTO)
+        {
+            //return mapHref;
+            return BdzWebsiteUtilities.BDZWebsiteUtilities.ParseMap(imageDTO.mapHref);
+        }
+    }
+}
---
+++ BDZService/BDZService/Controllers/PriceV2Controller.cs
@@ -0,0 +1,22 @@
+﻿using BdzWebsiteUtilities;
+using BDZWebsiteUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BDZService.Controllers
+{
+    public class PriceV2Controller : ApiController
+    {
+        // GET api/price
+        public PriceDTO Post([FromBody]SessionDTO SessionDTO, string id)
+        {
+            Cookie cookie = new Cookie("JSESSIONID", SessionDTO.sessionId);
+            cookie.Domain = "razpisanie.bdz.bg";
+            return BdzWebsiteUtilities.BDZWebsiteUtilities.ParcePrice(id, cookie);
+        }
+    }
+}
